12cfd7b94b3adea644d3a0d525acce1412a93b2a
Robert Locsin <59687824+roburp@users.noreply.github.com>
2024-10-01 14:20:11 +0800

    Complete the 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function (text, n) {
+  let output = "";
 
+  if (n < 0) return "ERROR";
+
+  for (let i = 0; i < n; i++) {
+    output += text;
+  }
+  return output;
 };
 
 // Do not edit below this line
