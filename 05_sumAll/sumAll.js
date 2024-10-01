a58b3f1ac61be2a95f74b4290bcd242790ac7857
Robert Locsin <59687824+roburp@users.noreply.github.com>
2024-10-01 15:31:34 +0800

    Complete 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let sum = 0;
+  let start = Math.min(num1, num2);
+  let end = Math.max(num1, num2);
 
+  if ([num1, num2].some((num) => typeof num !== "number" || Number.isNaN(num) || num < 0 || !Number.isInteger(num))) {
+    return "ERROR";
+  }
+
+  do {
+    sum += start;
+    start++;
+  } while (start <= end);
+
+  return sum;
 };
 
 // Do not edit below this line
