a2ab31909e3cae1a64139e195acddfc3be401df3
Robert Locsin <59687824+roburp@users.noreply.github.com>
2024-10-01 15:52:58 +0800

    Complete 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  // C = (F - 32) × 5/9
+  let celcius = (temp - 32) * (5 / 9);
+  celcius = Math.round(celcius * 10) / 10;
+  return celcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  // F = (9/5) C+32
+  let farenheit = (9 / 5) * temp + 32;
+  farenheit = Math.round(farenheit * 10) / 10;
+  return farenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
