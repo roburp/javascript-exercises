05108a3be3dcb3fdfdbe316c99851b5fa3a803b5
Robert Locsin <59687824+roburp@users.noreply.github.com>
2024-10-01 14:46:50 +0800

    Complete 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+  const reversedString = word.split("").reverse().join("");
+  return reversedString;
 };
 
 // Do not edit below this line
