9b678198378b958431a1bf6d15a4160d0114dd53
Robert Locsin <59687824+roburp@users.noreply.github.com>
2024-10-01 15:01:38 +0800

    Complete 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  const remove = Array.from(args);
+  const filteredArr = arr.filter((item) => !remove.includes(item));
+  return filteredArr;
 };
 
 // Do not edit below this line
